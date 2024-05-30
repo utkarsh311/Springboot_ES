d1aea99bfd90f89fe99d1adad52f26ba10bdea40
utkarsh311 <mksinha006@gmail.com>
2024-05-30 22:31:19 +0530

    CRUD operation ES Springboot

---
+++ SpringBootElasticsearchExample/src/main/java/com/example/springbootelasticsearchexample/controller/ProductController.java
@@ -0,0 +1,24 @@
+package com.example.springbootelasticsearchexample.controller;
+
+import com.example.springbootelasticsearchexample.entity.Product;
+import com.example.springbootelasticsearchexample.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/apis")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("/findAll")
+    Iterable<Product> findAll() {
+       return productService.getProducts();
+    }
+
+    @PostMapping("/insert")
+    Product insertProduct(@RequestBody Product product) {
+        return productService.insertProduct(product);
+    }
+}
---
+++ SpringBootElasticsearchExample/src/main/java/com/example/springbootelasticsearchexample/repo/ProductRepo.java
@@ -0,0 +1,10 @@
+package com.example.springbootelasticsearchexample.repo;
+
+import com.example.springbootelasticsearchexample.entity.Product;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+public interface ProductRepo extends ElasticsearchRepository<Product, Integer > {
+
+
+
+}
